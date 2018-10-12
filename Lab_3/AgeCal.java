10fbfc489901c45aca1f0a4d94cf94189ecfef72
ifathfatima <43847118+ifathfatima@users.noreply.github.com>
2018-10-12 16:51:01 +0530

    Add files via upload

---
+++ Lab_3/AgeCal.java
@@ -0,0 +1,103 @@
+package Lab_3;							//**********3.7 prog munni
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+ 
+public class AgeCal {
+ 
+public static void main(String[] args)
+{
+// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("First Name ");
+Person6 p=new Person6();
+String f=sc.next();
+System.out.println("last Name ");
+String l=sc.next();
+System.out.println("Gender ");
+char n=sc.next().charAt(0);
+//By getter and setter methods
+p.setFirstName(f);
+p.setLasttName(l);
+p.setGender(n);
+System.out.println("Enter the date of birth \n Year \n Month \n Day ");
+int year=sc.nextInt();
+int month=sc.nextInt();
+int dayOfMonth=sc.nextInt();
+LocalDate birthdate=LocalDate.of(year, month, dayOfMonth);
+LocalDate end = LocalDate.now();
+System.out.println("Person Details:");
+System.out.println("---------------------");
+ 
+System.out.println("First Name: "+p.getFirstName());
+System.out.println("Last Name: "+p.getLastName());
+System.out.println("Gender: "+p.getGender());
+p.caluculateAge(birthdate,end);
+p.getFullName(f, l);
+ 
+/*//passing values by constructor
+Person p1=new Person("Swetha","Pabbathi",'F');
+System.out.println("Person Details:");
+System.out.println("---------------------");
+ 
+System.out.println("First Name: "+p1.firstName);
+System.out.println("Last Name: "+p1.lastName);
+System.out.println("Gender: "+p1.Gender);*/
+ 
+ 
+ 
+ 
+}
+ 
+}
+  class Person6
+{
+String firstName,lastName;
+char Gender;
+Person6()
+{
+ 
+}
+Person6(String f,String l,char a)
+{
+firstName=f;
+lastName=l;
+Gender=a;
+}
+public void setFirstName(String s)
+{
+firstName=s;
+}
+public String getFirstName()
+{
+return firstName;
+}
+public void setLasttName(String s)
+{
+lastName=s;
+}
+public String getLastName()
+{
+return lastName;
+}
+public void setGender(char c)
+{
+Gender=c;
+}
+public char getGender()
+{
+return Gender;
+}
+void caluculateAge(LocalDate start,LocalDate end)
+{
+Period period = start.until(end);
+System.out.println( "Age: "+period.getYears()+"Years "+period.getMonths()+" Months "+period.getDays()+" Days");
+ 
+}
+void getFullName(String f,String l)
+{
+System.out.println("Full name: "+f+" "+l);
+}
+}
+ 
---
+++ Lab_3/ProductExpireDate.java
@@ -0,0 +1,35 @@
+package Lab_3;					//*********3.5 prog munni 
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+ 
+public class ProductExpireDate {
+ 
+void expireDate(LocalDate ld,Period pd)
+{
+LocalDate expdate=ld.plus(pd);
+//LocalDate expdate1=expdate.plusMonths(m);
+System.out.println("Expire date is(yyyy-mm-nn) "+ expdate);
+}
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter purchased \n year \n month \n days ");
+int yyyy=sc.nextInt();
+int mm=sc.nextInt();
+int dd=sc.nextInt();
+System.out.println("Enter period \n year \n month \n days ");
+int pyyyy=sc.nextInt();
+int pmm=sc.nextInt();
+int pdd=sc.nextInt();
+LocalDate purchasedate=LocalDate.of(yyyy,mm,dd);
+int py=2;
+int pm=4;
+Period period = Period.of(pyyyy,pmm,pdd);
+ProductExpireDate p=new ProductExpireDate();
+p.expireDate(purchasedate,period);
+ 
+}
+ 
+ 
+}
